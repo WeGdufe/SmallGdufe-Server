fc72c4bd6b5349586373b9fd191c683e8bd135ba
xiaoguang <wintercoder@gmail.com>
2017-03-27 10:11:02 +0800

    更正服务器地址

--- config/app_update.php
+++ config/app_update.php
@@ -6,10 +6,11 @@ return $update = [
         'forced' => false,
         'updateContent' => '1.实现Dr.com
 2.增加自动更新
+3.实现头像图标
         ',
-        'updateUrl' => 'http://www.wintercoder:82/index.php?r=work/update',
+        'updateUrl' => 'http://www.wintercoder.com:82/index.php?r=work/update',
         // 'updateUrl' => 'http://172.25.155.14:82/index.php?r=work/update',
-        'updateTime' => 1490270887,
+        'updateTime' => 1490270887, //版本发布的时间戳
         'versionCode' => 1,
         'versionName' =>  '1.0.0',
         'ignore' => false,
