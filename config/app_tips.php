fe9b6b609914f32c34a812f82e6d8825367a40d6
xiaoguang <wintercoder@gmail.com>
2017-04-17 22:26:30 +0800

    v1.1.0 实现每日启动Tips

---
+++ config/app_tips.php
@@ -0,0 +1,22 @@
+<?php
+
+return $appTips = [
+    'appTips' =>[
+        'version' => 1,
+        'enable' => true,
+        'title' => '一个人奋斗还是挺累的',
+        'message' => '有功能需求的话可以提下反馈，不过近期毕设答辩，会晚一点更新，另无薪招聘技术大佬呀~关于页面有联系方式',
+        'startTime' => '2017-04-17 20:58:22',
+        'endTime' => '2017-04-26 20:58:22',
+        'openUrl' => ''
+    ]
+];
+//         'openUrl' => 'http://www.wintercoder.com:8080/'
+
+        // "version":3,
+        // "enable":true,
+        // "title":"你的反馈是我更新的动力",
+        // "message":"有功能需求的话可以提下反馈，不过近期毕设答辩，会晚一点更新，另无薪招聘技术大佬呀~",
+        // "startTime":"2017-04-17 20:58:22",
+        // "endTime":"2017-04-17 22:59:28",
+        // "openUrl":""
--- inner_api/controllers/BaseController.php
+++ inner_api/controllers/BaseController.php
@@ -19,6 +19,7 @@ class BaseController extends Controller
 
     public function beforeAction($action)
     {
+        ini_set('max_execution_time','5');
         if (parent::beforeAction($action)) {
             $this->urlConst = Yii::$app->params;
             // Yii::warning("访问");
