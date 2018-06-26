12e4dd829d6543fa455a3cada26119138f71fac8
wintercoder <wintercoder@gmail.com>
2018-06-26 23:57:32 +0800

    remove apidoc dir

--- apidoc_gen.sh
+++ apidoc_gen.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-apidoc -i controllers/ -o apidoc/
- cp -r apidoc/* ../SmallGdufe-Web/apidoc/
+apidoc -i controllers/ -o ../SmallGdufe-Web/apidoc/
 # C:\Users\xiaoguang\AppData\Roaming\npm\apidoc -i controllers/ -o apidoc/
 # 注意不能命名为apidoc 避免无限递归
